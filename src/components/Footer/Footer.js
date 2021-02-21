9f12478100156337bb0e2c612823293e6467df0c
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-02-21 11:59:48 +0530

    dynamic footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,5 @@
+// React router
+import { useLocation } from 'react-router-dom';
 // mui imports
 import { Paper, Typography } from '@material-ui/core';
 // styles
@@ -6,12 +8,24 @@ import useStyles from './styles';
 const Footer = () => {
   // mui
   const classes = useStyles();
+  // location
+  const location = useLocation();
   return (
-    <Paper elevation={8}>
-      <Typography className={classes.BottomGreet} variant='h6'>
-        wow you reached the bottom!
-      </Typography>
-    </Paper>
+    <>
+      {location.pathname == '/auth' ? (
+        <Paper elevation={8}>
+          <Typography className={classes.BottomGreet} variant='h6'>
+            Hello friendly citizen.. sup?
+          </Typography>
+        </Paper>
+      ) : (
+        <Paper elevation={8}>
+          <Typography className={classes.BottomGreet} variant='h6'>
+            wow you scroll too much!
+          </Typography>
+        </Paper>
+      )}
+    </>
   );
 };
 
